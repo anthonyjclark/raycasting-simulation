5625031da5f362db1f291756d6e76e167bdca662
Anthony Clark <anthonyjclark@gmail.com>
2020-12-17 11:55:48 -0800

    Updated image saving code and a some variable renaming.

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -4,105 +4,107 @@
 #include <iostream>
 #include <vector>
 
-std::string imgDir;
+// TODO: remove as global variable
+std::string imageDirectory;
 
 /*
  * Saves an image from the window for the specified move
- * assumes imgDir doesn't include '/'
- * assumes imgDir/move directory already exists
+ * assumes imageDirectory doesn't include '/'
+ * assumes imageDirectory/move directory already exists
  *
  * @param w RaycastWorld
  * @param move specified move and subdirectory to save image to
  */
-void saveImg(RaycastWorld *w, std::string move)
+void saveCommandPNG(RaycastWorld *world, std::string move)
 {
     static int count = 0;
-    if (move == "q")
-    {
-        std::cout << "Total moves " + std::to_string(count) + "\n";
-    }
-    else
-    {
-        std::string imgPath = imgDir + "/" + move + "/" + std::to_string(count) + ".png";
-        w->savePNG(imgPath);
-        count++;
-    }
+    std::string imagePath = imageDirectory + "/" + move + "/" + std::to_string(count) + ".png";
+    std::cout << "Saving image \"" << imagePath << "\"\n";
+    world->savePNG(imagePath);
+    count++;
 }
 
 // void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
 void keyCallback(GLFWwindow *window, int key, int, int action, int)
 {
-    std::cout << key << std::endl;
+    auto world = static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window));
+
+    if (imageDirectory.length() > 0 && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    {
+        if (key == GLFW_KEY_UP)
+        {
+            saveCommandPNG(world, "forward");
+        }
+        else if (key == GLFW_KEY_LEFT)
+        {
+            saveCommandPNG(world, "left");
+        }
+        else if (key == GLFW_KEY_RIGHT)
+        {
+            saveCommandPNG(world, "right");
+        }
+    }
+
     if (key == GLFW_KEY_Q && action == GLFW_PRESS)
     {
-        saveImg(static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window)), "q");
         glfwSetWindowShouldClose(window, GLFW_TRUE);
     }
-    else if (key == GLFW_KEY_UP && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    else if (key == GLFW_KEY_UP && action == GLFW_PRESS)
     {
-        saveImg(static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window)), "forward");
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setWalk(FORWARD);
+        world->setWalk(FORWARD);
     }
-    else if (key == GLFW_KEY_DOWN && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
     {
-        saveImg(static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window)), "backward");
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setWalk(BACKWARD);
+        world->setWalk(BACKWARD);
     }
     else if ((key == GLFW_KEY_UP || key == GLFW_KEY_DOWN) && action == GLFW_RELEASE)
     {
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setWalk(STOPPED);
+        world->setWalk(STOPPED);
     }
-    else if (key == GLFW_KEY_RIGHT && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    else if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
     {
-        saveImg(static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window)), "right");
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setTurn(RIGHT);
+        world->setTurn(RIGHT);
     }
-    else if (key == GLFW_KEY_LEFT && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    else if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
     {
-        saveImg(static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window)), "left");
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setTurn(LEFT);
+        world->setTurn(LEFT);
+    }
+    else if ((key == GLFW_KEY_LEFT || key == GLFW_KEY_RIGHT) && action == GLFW_RELEASE)
+    {
+        world->setTurn(STOP);
     }
     else if (key == GLFW_KEY_RIGHT_SHIFT && action == GLFW_PRESS)
     {
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setZ(-100);
+        world->setZ(-100);
     }
     else if (key == GLFW_KEY_RIGHT_SHIFT && action == GLFW_RELEASE)
     {
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setZ(0);
+        world->setZ(0);
     }
     else if (key == GLFW_KEY_P && action == GLFW_PRESS)
     {
-        auto world = static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window));
         std::cout << world->getX() << ", " << world->getY() << std::endl;
     }
     else if (key == GLFW_KEY_M && action == GLFW_PRESS)
     {
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->toggleMiniMap();
-    }
-    else if ((key == GLFW_KEY_LEFT || key == GLFW_KEY_RIGHT) && action == GLFW_RELEASE)
-    {
-        static_cast<RaycastWorld *>(glfwGetWindowUserPointer(window))->setTurn(STOP);
+        world->toggleMiniMap();
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    // const unsigned int WINDOW_WIDTH = 640;
-    // const unsigned int WINDOW_HEIGHT = 480;
-    const unsigned int WINDOW_WIDTH = 320;
-    const unsigned int WINDOW_HEIGHT = 240;
+    const unsigned int DEFAULT_WINDOW_WIDTH = 320;
+    const unsigned int DEFAULT_WINDOW_HEIGHT = 240;
+    const auto DEFAULT_WORLD_FILE = "../Worlds/maze.txt";
 
-    // static int count = std::stoul(argv[1]);
-    std::string mapFile = argc >= 2 ? argv[1] : "../Worlds/maze.txt";
-    imgDir = argc >= 3 ? argv[2] : "../Images";
-
-    usize width = argc >= 4 ? std::stoul(argv[3]) : WINDOW_WIDTH;
-    usize height = argc >= 5 ? std::stoul(argv[4]) : WINDOW_HEIGHT;
+    // Process program arguments (must be given in this order)
+    std::string worldFilepath = argc >= 2 ? argv[1] : DEFAULT_WORLD_FILE;
+    imageDirectory = argc >= 3 ? argv[2] : "";
+    usize width = argc >= 4 ? std::stoul(argv[3]) : DEFAULT_WINDOW_WIDTH;
+    usize height = argc >= 5 ? std::stoul(argv[4]) : DEFAULT_WINDOW_HEIGHT;
 
     DisplayArray displayer(width, height, keyCallback);
-    RaycastWorld world(width, height, mapFile);
-    world.setDirection(0);
-    world.setPosition(3.5, 13.5);
+    RaycastWorld world(width, height, worldFilepath);
 
     glfwSetWindowUserPointer(displayer.window, &world);
 
@@ -110,8 +112,10 @@ int main(int argc, char const *argv[])
     while (displayer.running())
     {
         displayer.pre();
+
         world.updatePose();
         displayer.render(world.getBuffer());
+
         displayer.post();
 
         //std::cout << 1 / (glfwGetTime() - t) << std::endl;
